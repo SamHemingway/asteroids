69c67df8e8d855f5384981d06c6cef093267f6a0
SamHemingway <hello@samhemingway.dev>
2024-11-12 08:45:27 -0500

    feat: added collision detection

--- main.py
+++ main.py
@@ -37,6 +37,11 @@ def main():
     
     pygame.display.flip()
 
+    for asteroid in asteroids:
+      if asteroid.is_colliding_with(player):
+        print("Game over!")
+        exit()
+
     dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
